44c69b752489f6492b30c82ceade2d457a487b28
Favour Godwin Arua <favourvalg@gmail.com>
2020-12-12 09:40:27 +0100

    header and footer update

---
+++ layout/footer.php
@@ -0,0 +1,176 @@
+<section class="section bg-primary border-0 m-0">
+				<div class="container">
+					<div class="row justify-content-center text-center text-lg-left py-4">
+						<div class="col-lg-auto appear-animation" data-appear-animation="fadeInRightShorter">
+							<div class="feature-box feature-box-style-2 d-block d-lg-flex mb-4 mb-lg-0">
+								<div class="feature-box-icon">
+									<i class="icon-location-pin icons text-color-light"></i>
+								</div>
+								<div class="feature-box-info pl-1">
+									<h5 class="font-weight-light text-color-light opacity-7 mb-0">ADDRESS</h5>
+									<p class="text-color-light font-weight-semibold mb-0">A Close, 5th Avenue, Gwarimpa Estate,
+Abuja, Nigeria<br>MON - FRI: 10:00am - 6:00pm
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-auto appear-animation" data-appear-animation="fadeInRightShorter"
+							data-appear-animation-delay="200">
+							<div class="feature-box feature-box-style-2 d-block d-lg-flex mb-4 mb-lg-0 px-xl-4 mx-lg-5">
+								<div class="feature-box-icon">
+									<i class="icon-call-out icons text-color-light"></i>
+								</div>
+								<div class="feature-box-info">
+									<h5 class="font-weight-light text-color-light opacity-7 mb-0">CALL US NOW</h5>
+									<a href="tel:+2347013669823"
+										class="text-color-light font-weight-semibold text-decoration-none">7013669823</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-auto appear-animation" data-appear-animation="fadeInRightShorter"
+							data-appear-animation-delay="400">
+							<div class="feature-box feature-box-style-2 d-block d-lg-flex">
+								<div class="feature-box-icon">
+									<i class="icon-share icons text-color-light"></i>
+								</div>
+								<div class="feature-box-info pl-1">
+									<h5 class="font-weight-light text-color-light opacity-7 mb-0">FOLLOW US</h5>
+									<p class="mb-0">
+										<span class="social-icons-facebook"><a href="https://www.facebook.com/"
+												target="_blank" class="text-color-light font-weight-semibold"
+												title="Facebook"><i class="mr-1 fab fa-facebook-f"></i>
+												FACEBOOK</a></span>
+										<span class="social-icons-twitter pl-3"><a href="https://www.twitter.com/"
+												target="_blank" class="text-color-light font-weight-semibold"
+												title="Twitter"><i class="mr-1 fab fa-twitter"></i> TWITTER</a></span>
+										<span class="social-icons-instagram pl-3"><a href="https://www.linkedin.com/"
+												target="_blank" class="text-color-light font-weight-semibold"
+												title="Linkedin"><i class="mr-1 fab fa-instagram"></i>
+												INSTAGRAM</a></span>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+		<footer id="footer" class="mt-0">
+			<div class="footer-copyright">
+				<div class="container py-2">
+					<div class="row py-4">
+						<div class="col d-flex align-items-center justify-content-center">
+							<p><strong>GreyLux</strong> - © Copyright 2020. All Rights Reserved.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</div>
+
+	<script src="vendor/jquery/jquery.min.js"></script>
+	<script src="vendor/jquery.appear/jquery.appear.min.js"></script>
+	<script src="vendor/jquery.easing/jquery.easing.min.js"></script>
+	<script src="vendor/jquery.cookie/jquery.cookie.min.js"></script>
+	<script src="master/style-switcher/style.switcher.js" id="styleSwitcherScript" data-base-path=""
+		data-skin-src=""></script>
+	<script src="vendor/popper/umd/popper.min.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="vendor/common/common.min.js"></script>
+	<script src="vendor/jquery.validation/jquery.validate.min.js"></script>
+	<script src="vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
+	<script src="vendor/jquery.gmap/jquery.gmap.min.js"></script>
+	<script src="vendor/jquery.lazyload/jquery.lazyload.min.js"></script>
+	<script src="vendor/isotope/jquery.isotope.min.js"></script>
+	<script src="vendor/owl.carousel/owl.carousel.min.js"></script>
+	<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+	<script src="vendor/vide/jquery.vide.min.js"></script>
+	<script src="vendor/vivus/vivus.min.js"></script>
+
+	<script src="js/theme.js"></script>
+
+	<script src="js/views/view.contact.js"></script>
+
+	<script src="js/theme.init.js"></script>
+
+	<script src="js/examples/examples.portfolio.js"></script>
+	<script>
+		(function (i, s, o, g, r, a, m) {
+			i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+				(i[r].q = i[r].q || []).push(arguments)
+			}, i[r].l = 1 * new Date(); a = s.createElement(o),
+				m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+		})(window, document, 'script', 'http://www.google-analytics.com/analytics.js', 'ga');
+
+		ga('create', 'UA-42715764-5', 'auto');
+		ga('send', 'pageview');
+	</script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAhpYHdYRY2U6V_VfyyNtkPHhywLjDkhfg"></script>
+	<script>
+
+		/*
+		Map Settings
+
+			Find the Latitude and Longitude of your address:
+				- https://www.latlong.net/
+				- http://www.findlatitudeandlongitude.com/find-address-from-latitude-and-longitude/
+
+		*/
+
+		// Map Markers
+		var mapMarkers = [{
+			address: "New York, NY 10017",
+			html: "<strong>New York Office</strong><br>New York, NY 10017<br><br><a href='#' onclick='mapCenterAt({latitude: 40.75198, longitude: -73.96978, zoom: 16}, event)'>[+] zoom here</a>",
+			icon: {
+				image: "img/pin.png",
+				iconsize: [26, 46],
+				iconanchor: [12, 46]
+			}
+		}];
+
+		// Map Initial Location
+		var initLatitude = 40.75198;
+		var initLongitude = -73.96978;
+
+		// Map Extended Settings
+		var mapSettings = {
+			controls: {
+				draggable: (($.browser.mobile) ? false : true),
+				panControl: true,
+				zoomControl: true,
+				mapTypeControl: true,
+				scaleControl: true,
+				streetViewControl: true,
+				overviewMapControl: true
+			},
+			scrollwheel: false,
+			markers: mapMarkers,
+			latitude: initLatitude,
+			longitude: initLongitude,
+			zoom: 5
+		};
+
+		var map = $('#googlemaps').gMap(mapSettings),
+			mapRef = $('#googlemaps').data('gMap.reference');
+
+		// Map text-center At
+		var mapCenterAt = function (options, e) {
+			e.preventDefault();
+			$('#googlemaps').gMap("centerAt", options);
+		}
+
+		// Styles from https://snazzymaps.com/
+		var styles = [{ "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#e9e9e9" }, { "lightness": 17 }] }, { "featureType": "landscape", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 20 }] }, { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [{ "color": "#ffffff" }, { "lightness": 17 }] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [{ "color": "#ffffff" }, { "lightness": 29 }, { "weight": 0.2 }] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 18 }] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 16 }] }, { "featureType": "poi", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 21 }] }, { "featureType": "poi.park", "elementType": "geometry", "stylers": [{ "color": "#dedede" }, { "lightness": 21 }] }, { "elementType": "labels.text.stroke", "stylers": [{ "visibility": "on" }, { "color": "#ffffff" }, { "lightness": 16 }] }, { "elementType": "labels.text.fill", "stylers": [{ "saturation": 36 }, { "color": "#333333" }, { "lightness": 40 }] }, { "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "transit", "elementType": "geometry", "stylers": [{ "color": "#f2f2f2" }, { "lightness": 19 }] }, { "featureType": "administrative", "elementType": "geometry.fill", "stylers": [{ "color": "#fefefe" }, { "lightness": 20 }] }, { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [{ "color": "#fefefe" }, { "lightness": 17 }, { "weight": 1.2 }] }];
+
+		var styledMap = new google.maps.StyledMapType(styles, {
+			name: 'Styled Map'
+		});
+
+		mapRef.mapTypes.set('map_style', styledMap);
+		mapRef.setMapTypeId('map_style');
+
+	</script>
+	<a href="index-2.html#demos" class="go-to-demos"><i class="fas fa-arrow-left"></i> More Demos</a>
+</body>
+
+</html>
---
+++ layout/header.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>GreyLux</title>
+	<meta name="keywords" content="HTML5 Template" />
+	<meta name="description" content="GreyLux">
+	<meta name="author" content="GreyLux">
+
+	<link rel="shortcut icon" href="img/logos/logo.png" type="image/x-icon" />
+	<link rel="apple-touch-icon" href="img/logos/logo.png">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
+
+	<link
+		href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800%7CShadows+Into+Light%7CPlayfair+Display:400&amp;display=swap"
+		rel="stylesheet" type="text/css">
+
+	<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="vendor/fontawesome-free/css/all.min.css">
+	<link rel="stylesheet" href="vendor/animate/animate.compat.css">
+	<link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.min.css">
+	<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.carousel.min.css">
+	<link rel="stylesheet" href="vendor/owl.carousel/assets/owl.theme.default.min.css">
+	<link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.min.css">
+
+	<link rel="stylesheet" href="css/theme.css">
+	<link rel="stylesheet" href="css/theme-elements.css">
+	<link rel="stylesheet" href="css/theme-blog.css">
+	<link rel="stylesheet" href="css/theme-shop.css">
+	<link rel="stylesheet" href="css/skins/default.css">
+	<script src="master/style-switcher/style.switcher.localstorage.js"></script>
+
+	<link rel="stylesheet" href="css/custom.css">
+
+	<script src="vendor/modernizr/modernizr.min.js"></script>
+</head>
+
+<body class="one-page loading-overlay-showing" data-target="#header" data-spy="scroll" data-offset="100"
+	data-plugin-page-transition data-loading-overlay data-plugin-options="{'hideDelay': 500}">
+	<div class="loading-overlay">
+		<div class="bounce-loader">
+			<div class="bounce1"></div>
+			<div class="bounce2"></div>
+			<div class="bounce3"></div>
+		</div>
+	</div>
+	<div class="body">
+		<header id="header" class="header-transparent header-effect-shrink"
+			data-plugin-options="{'stickyEnabled': true, 'stickyEffect': 'shrink', 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': true, 'stickyChangeLogo': true, 'stickyStartAt': 30, 'stickyHeaderContainerHeight': 70}">
+			<div class="header-body border-top-0 bg-dark box-shadow-none">
+				<div class="header-container container">
+					<div class="header-row">
+						<div class="header-column">
+							<div class="header-row">
+								<div class="header-logo">
+									<a href="index.php">
+										<img alt="Greylux" width="80" height="80" src="img/logos/logo.png">
+									</a>
+								</div>
+							</div>
+						</div>
+						<div class="header-column justify-content-end">
+							<div class="header-row">
+								<div
+									class="header-nav header-nav-links header-nav-dropdowns-dark header-nav-light-text order-2 order-lg-1">
+									<div
+										class="header-nav-main header-nav-main-mobile-dark header-nav-main-square header-nav-main-dropdown-no-borders header-nav-main-effect-2 header-nav-main-sub-effect-1">
+										<nav class="collapse">
+											<ul class="nav nav-pills" id="mainNav">
+												<li class="nav-item">
+													<a data-hash class="nav-link"
+														href="index.php">
+														Home
+													</a>
+												</li>
+												<li class="dropdown">
+													<a class="dropdown-item dropdown-toggle" data-hash data-hash-offset="68"
+														href="#services">Services</a>
+													<ul class="dropdown-menu">
+														<li><a class="dropdown-item" href="#">Investment</a></li>
+														<li><a class="dropdown-item" href="#">Real Estate</a></li>
+														<li><a class="dropdown-item" href="#">IT Solutions and Consultancy</a></li>
+														<li><a class="dropdown-item" href="#">General Merchandise</a></li>
+														<li><a class="dropdown-item" href="#">General Contractors</a></li>
+													</ul>
+												</li>
+												<li>
+													<a class="dropdown-item" data-hash data-hash-offset="68"
+														href="#projects">Projects</a>
+												</li>
+												<li>
+													<a class="dropdown-item" data-hash data-hash-offset="68"
+														href="#clients">Clients</a>
+												</li>
+												<li>
+													<a class="dropdown-item" data-hash data-hash-offset="68"
+														href="#team">Meet the Team</a>
+												</li>
+												<li>
+													<a class="dropdown-item" data-hash data-hash-offset="68"
+														href="#contact">Contact Us</a>
+												</li>
+											</ul>
+										</nav>
+									</div>
+									<button class="btn header-btn-collapse-nav" data-toggle="collapse"
+										data-target=".header-nav-main nav">
+										<i class="fas fa-bars"></i>
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
